5280effedeb4381ad61e32d3ad0c5d0222159b52
Davi6769 <matematicaparaburros6769@gmail.com>
2026-08-08 12:47:05 -0300

    melhorei a calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,64 +1,95 @@
-#FUNÇÕES
-def escolha1(operacao):
-    n1 = int(input("digite um numero: "))
-    n2 = int(input("digite outro numero: "))
-    if operacao == 1:
-        print(f"{n1} + {n2} = {n1 + n2}")
-    elif operacao == 2:
-        print(f"{n1} - {n2} = {n1 - n2}")
-    elif operacao == 3:
-        print(f"{n1} X {n2} = {n1 * n2}")
-    elif operacao == 4:
-        if n1 == 0:
-            print("não é possivel dividir 0")
-        else:
-            print(f"{n1} / {n2} = {n1 / n2}")
+from colorama import Fore, Style
+from time import sleep
 
-def repeticao(operacao):
-    while True:
-        pergunta = input("gostaria de repetir o codigo ? [y/n]: ")
-        while pergunta not in ["y", "n"]:
-            print("resposta invalida")
-            pergunta = input("gostaria de repetir o codigo ? [y/n]: ")
-        if pergunta == "y":
-            escolha1(operacao)
-        else:
-            break
+opcoes = ["(1)-Soma", "(2)-Subtração", "(3)-Multiplicação", "(4)-Divisão", "(X)-Sair"]
+
+
+def voltar():
+    volta = input("Digite qualuqer tecla para voltar a area inicial: ")
+
+def menu():
+    for i in opcoes:
+        print(f"\n{i}")
+
+    escolha = input("\nEscolha digitando somente o numero da opção desejada: ").lower()
+
+    while escolha not in ["1", "2", "3", "4", "x"]:
+        print(f"{Fore.RED}Resposta invalida{Style.RESET_ALL}")
+        escolha = input("Escolha digitando somente o numero da opção desejada: ")
+    return escolha
+  
+
+def numeros():
+    numero1 = int(input("Digite um numero: "))
+    numero2 = int(input("Digite outro numero: "))
+    return numero1, numero2
+
+def soma():
+    print("\nVocê escolheu soma")
+    numero1, numero2 = numeros()
+    resposta = numero1 + numero2
+    print(f"A soma de {numero1} por {numero2} é igual a {resposta}")
+    print(f"{numero1} + {numero2} = {resposta}")
+    voltar()
+
+def subtracao():
+    print("\nVocê escolheu subtração")
+    numero1, numero2 = numeros()
+    resposta = numero1 - numero2
+    print(f"{numero1} - {numero2} = {resposta}")
+    voltar()
+
+def multiplicacao():
+    print("\nVocê escolheu multiplicação")
+    numero1, numero2 = numeros()
+    resposta = numero1 * numero2
+    print(f"{numero1} X {numero2} = {resposta}")
+    voltar()
+
+def divisão():
+    print("\nVocê escolheu divisão")
+    numero1, numero2 = numeros()
+    while numero2 == 0:
+        print(f"{Fore.RED}Resposta invalida: imposivel dividir por zero{Style.RESET_ALL}")
+        numero1, numero2 = numeros()
+    resposta = numero1 / numero2
+    print(f"{numero1} / {numero2} = {resposta}")
+    voltar()
 
 
 #CALCULADORA
+
+
+titulo = """
+      /$$$$$$            /$$                     /$$                 /$$                             
+     /$$__  $$          | $$                    | $$                | $$                             
+    | $$  \__/  /$$$$$$ | $$  /$$$$$$$ /$$   /$$| $$  /$$$$$$   /$$$$$$$  /$$$$$$   /$$$$$$  /$$$$$$ 
+    | $$       |____  $$| $$ /$$_____/| $$  | $$| $$ |____  $$ /$$__  $$ /$$__  $$ /$$__  $$|____  $$
+    | $$        /$$$$$$$| $$| $$      | $$  | $$| $$  /$$$$$$$| $$  | $$| $$  \ $$| $$  \__/ /$$$$$$$
+    | $$    $$ /$$__  $$| $$| $$      | $$  | $$| $$ /$$__  $$| $$  | $$| $$  | $$| $$      /$$__  $$
+    |  $$$$$$/|  $$$$$$$| $$|  $$$$$$$|  $$$$$$/| $$|  $$$$$$$|  $$$$$$$|  $$$$$$/| $$     |  $$$$$$$
+    \______/  \_______/|__/ \_______/ \______/ |__/ \_______/ \_______/ \______/ |__/      \_______/
+    """
+
 while True:
-    print()
-    print("CALCULADORA")
-    opcoes = ["1-Soma", "2-Subtração", "3-Multiplicação", "4-Divisão", "0-sair"]
-    for itens in opcoes:
-        print(itens)
-    escolha = int(input("Escolha a operação digitando o numero que esta na frente dele: "))
 
-    while escolha not in [1, 2, 3, 4, 0]:
-        print("resposta invalida")
-        escolha = int(input("Escolha a operação digitando o numero que esta na frente dele: "))
+    print("=" * 40)
+    print(f"\n{titulo}\n")
+    print("=" * 40)
 
-    if escolha == 1:
-        print("Você escolheu SOMA")
-        escolha1(1)
-        repeticao(1)
+    escolha = menu()
 
-    elif escolha == 2:
-        print("Você escolheu SUBTRAÇÃO")
-        escolha1(2)
-        repeticao(2)
-    
-    elif escolha == 3:
-        print("Você escolheu MULTIPLICAÇÃO")
-        escolha1(3)
-        repeticao(3)
-    
-    elif escolha == 4:
-        print("Você escolheu DIVISÃO")
-        escolha1(4)
-        repeticao(4)
+    if escolha == "1":
+        soma()
+
+    elif escolha == "2":
+        subtracao()
+
+    elif escolha == "3":
+        multiplicacao()
+
+    elif escolha == "4":
+        divisão()
+
     else:
         break
-
-
